f6246579ccdde41750fdecd3201bb1dbcb948a92
maria montenegro <memtmontenegro@gmail.com>
2025-03-07 16:52:20 -0300

    productdestroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
+before_action :set_product, only: [:show, :destroy]
 
   def index
     @products = Product.all
@@ -40,6 +40,6 @@ class ProductsController < ApplicationController
   end
 
   def set_product
-    @product = Product.find(params.expect(:id))
+    @product = Product.find(params[:id])
   end
 end
