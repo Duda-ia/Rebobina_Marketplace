714f73eed05409830621ac7a34b115d33ad5eb21
kaue ogibowski <kaueogibowski@outlook.com>
2025-03-07 18:08:25 -0300

    new order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,21 @@
 class OrdersController < ApplicationController
   def index
   end
+
+  def new
+    @product = Product.find(params[:product_id])
+    @order = Order.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @order = Order.new
+    @order.user = current_user
+    @order.product = @product
+    if @order.save
+      redirect_to products_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
