e3e544eb93aa54b780381735f6be418bdc57eec2
beatriz braga <biabbraga@gmail.com>
2025-03-07 15:41:21 -0300

    create show products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,16 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show ]
+
+  def show
+  end
+
+  private
+  def set_product
+    @product = Product.find(params.expect(:id))
+  end
+
+  def product_params
+    params.expect(product: [ :category, :name, :description, :price, :user_id ])
+  end
+
 end
